52a73f304124e2558fe424ce680d47aa18d02aca
Edrick Ngouyassa <60397566+eddyngojr@users.noreply.github.com>
2020-03-17 00:04:32 -0400

    Add files via upload

---
+++ codequiz.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+     Javascript Code Quiz
+    </title>
+    <!-- [CSS AND JS] -->
+    <link href="js-quiz.css" rel="stylesheet">
+    <script src="js-quiz-questions.js"></script>
+    <script src="js-quiz.js"></script>
+  </head>
+  <body>
+    <!-- [QUIZ CONTAINER] -->
+    <form id="quiz-wrap"></form>
+  </body>
+</html>
+
+<script/>
+
+var questions = [
+  {
+    q : "What is the standard distance between the target and archer in Olympics?",
+    o : [
+      "50 meters",
+      "70 meters",
+      "100 meters",
+      "120 meters"
+    ],
+    a : 1 
+  },
+  {
+    q : "Which is the highest number on a standard roulette wheel?",
+    o : [
+      "22",
+      "24",
+      "32",
+      "36"
+    ],
+    a : 3
+  },
+  {
+    q : "How much wood could a woodchuck chuck if a woodchuck would chuck wood?",
+    o : [
+      "400 pounds",
+      "550 pounds",
+      "700 pounds",
+      "750 pounds"
+    ],
+    a : 2
+  },
+  {
+    q : "Which is the seventh planet from the sun?",
+    o : [
+      "Uranus",
+      "Earth",
+      "Pluto",
+      "Mars"
+    ],
+    a : 0
+  },
+  {
+    q : "Which is the largest ocean on Earth?",
+    o : [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean"
+    ],
+    a : 3
+  }
+];
